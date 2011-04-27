0bd4048cff57d0c402bc09ba13308b65b812112f
ptvutp@gmail.com <ptvutp@gmail.com@6b46fa10-d81d-2889-7adf-f52263feaee9>
2011-04-27 11:37:32 +0000

    Test JUnit

---
+++ trunk/AutoTrading/testSrc/test/AssetManager.java
@@ -0,0 +1,51 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AssetManager {
+
+	AssetManager manager = new AssetManager();
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAssetManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAssetByID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllAssets() {
+		fail("Not yet implemented");
+	}
+
+}
