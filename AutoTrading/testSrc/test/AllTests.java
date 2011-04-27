bffcfa6176da623cc11664557cbaea9d5a6d2264
ptvutp@gmail.com <ptvutp@gmail.com@6b46fa10-d81d-2889-7adf-f52263feaee9>
2011-04-27 11:37:32 +0000

    Test JUnit

---
+++ AutoTrading/testSrc/test/AllTests.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AssetManager.class,
+        AssetManager.class
+})
+public class AllTests {
+}
