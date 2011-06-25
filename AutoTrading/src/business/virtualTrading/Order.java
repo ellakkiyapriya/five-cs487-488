c65be2ae74e0d1dd93237a05af5900c9172ad3f9
lvxngoc@gmail.com <lvxngoc@gmail.com@6b46fa10-d81d-2889-7adf-f52263feaee9>
2011-06-25 00:06:15 +0000

    virtual trading version 1.5 : redesign class diagram

--- AutoTrading/src/business/virtualTrading/Order.java
+++ AutoTrading/src/business/virtualTrading/Order.java
@@ -1,87 +1,6 @@
 package business.virtualTrading;
 
-/* @Dinh : put getLatestDate() in PorfolioManager.java 
- * 		and	  execute() in OrderEntity.java	
- * 
- public Date getLatestDate() {
- try {
- Date date = null;
- String queryString = "SELECT max(date) as date FROM portfolio";
- connection = getConnection();
- ptmt = connection.prepareStatement(queryString);
- resultSet = ptmt.executeQuery();
 
- if (resultSet.next()) {
- date = resultSet.getDate("date"); 
- }
-
- return date;
- } catch (SQLException e) {
- e.printStackTrace();
- } finally {
- try {
- if (resultSet != null)
- resultSet.close();
- if (ptmt != null)
- ptmt.close();
- if (connection != null)
- connection.close();
- } catch (SQLException e) {
- e.printStackTrace();
- } catch (Exception e) {
- e.printStackTrace();
- }
-
- }
- return null;
- }
- */
-
-/*
- public void execute() {
-
- // Update user's cash
-
- UserManager userManager = new UserManager();
- UserEntity user = userManager.getUserByID(userID);
- if (this.orderType)
- user.setCash(user.getCash() - price*volume);
- else
- user.setCash(user.getCash() + price*volume);
-
-
- // Update user's portfolio 
-
- PortfolioManager portfolioManager = new PortfolioManager();
- PortfolioEntity portfolioTemp;
- Double priceTemp;
- Double volumeTemp;
- ArrayList<PortfolioEntity> portfolios = portfolioManager.getPortfolioByDate(portfolioManager.getLatestDate());
- for(int i = 0; i < portfolios.size(); i++) {
- portfolioTemp = portfolios.get(i);
- portfolioTemp.setDate(date);
- if (portfolioTemp.getAssetID() == assetID) {
- if (orderType) { // buy
- priceTemp = (portfolioTemp.getPrice()* portfolioTemp.getVolume() + price*volume )/ (portfolioTemp.getVolume()+volume);
- volumeTemp = portfolioTemp.getVolume() + volume;
- portfolioTemp.setPrice(priceTemp);
- portfolioTemp.setVolume(volumeTemp);
- portfolioManager.add(portfolioTemp);
- } else { // sell
- if(portfolioTemp.getVolume() > volume) {
- priceTemp = (portfolioTemp.getPrice()* portfolioTemp.getVolume() - price*volume )/ (portfolioTemp.getVolume()+volume);
- volumeTemp = portfolioTemp.getVolume() - volume;
- portfolioTemp.setPrice(priceTemp);
- portfolioTemp.setVolume(volumeTemp);
- portfolioManager.add(portfolioTemp);
- }
- }
- } else
- portfolioManager.add(portfolioTemp);
- }
- }
-
- */
 
 import java.sql.Date;
 import java.sql.SQLException;
@@ -91,102 +10,58 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import dataAccess.databaseManagement.entity.AssetEntity;
 import dataAccess.databaseManagement.entity.OrderEntity;
+import dataAccess.databaseManagement.entity.UserEntity;
 import dataAccess.databaseManagement.manager.AssetManager;
 import dataAccess.databaseManagement.manager.OrderManager;
+import dataAccess.databaseManagement.manager.UserManager;
 
 /**
  * Class Name: Order
  * 
- * @version 1.1
+ * @version 1.5
  * @date June 1, 2011
  * @author Xuan Ngoc
  */
 
 /*
  * TODO : Assume that names of all symbols are unique
- * 
- * public long getAssetIDbySymbol(String assetSymbol) { long assetID = -1; try {
- * connection = getConnection(); Statement statement =
- * connection.createStatement(); resultSet =
- * statement.executeQuery("SELECT asset_id FROM asset WHERE symbol=? ");
- * resultSet.next(); assetID = resultSet.getSymbol(1);
- * 
- * } catch (SQLException ex) { ex.printStackTrace(); } return assetID; }
  */
 
 public class Order {
 
-	private String assetSymbol;
+	private Asset asset;
 	private boolean orderType;
 	private double price;
 	private double volume;
 	private double value;
+	private boolean matched;
 
 	public Order() {
 
 	}
 
-	public Order(String assetSymbol, boolean orderType, double price,
-			double volume) {
-		this.setAssetSymbol(assetSymbol);
+	public Order(Asset asset, boolean orderType, double price, double volume) {
+		this.setAsset(asset);
 		this.setOrderType(orderType);
 		this.setPrice(price);
 		this.setVolume(volume);
 		this.setValue(price * volume);
+		this.setMatched(false);
 	}
-
-	/**
-	 * Add order to database. <li>Note: this method update database
-	 */
 	
-/*	// add order to database by getAssetIDbySymbol()
- *  public void add(long userID) {
- *  AssetManager assetManager = new AssetManager();
- *  long assetID = assetManager.getAssetIDbySymbol();
-*/ 
- 	
- 	public void add(long userID, long assetID) {
- 		Calendar cal = Calendar.getInstance();
- 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
- 		Date date = Date.valueOf(dateFormat.format(cal.getTime()));
- 		
-		OrderManager orderManager = new OrderManager();
-		OrderEntity orderEntity = new OrderEntity(orderType, userID,date , assetID, price, volume, true);
-		orderManager.add(orderEntity);
+	public Order(AssetEntity asset, boolean orderType, double price, double volume) {
+		this.asset = new Asset(asset);
+		this.setOrderType(orderType);
+		this.setPrice(price);
+		this.setVolume(volume);
+		this.setValue(price * volume);
+		this.setMatched(false);
 	}
 
-	/**
-	 * Get orders by Date from database.
-	 * 
-	 * @param date
-	 * @return
-	 */
-	public ArrayList<Order> getOrderByDate(Date date) {
-		OrderManager orderManager = new OrderManager();
-		AssetManager assetmanager = new AssetManager();
-		ArrayList<OrderEntity> orderEntityList = orderManager
-				.getOrderByDate(date);
-		ArrayList<Order> orderList = new ArrayList<Order>();
-		OrderEntity currentOrder;
-		String currentAssetSymbol;
-
-		for (int i = 0; i < orderEntityList.size(); i++) {
-			currentOrder = orderEntityList.get(i);
-			currentAssetSymbol = assetmanager.getAssetByID(
-					currentOrder.getAssetID()).getSymbol();
-			Order order = new Order(currentAssetSymbol, currentOrder
-					.getOrderType(), currentOrder.getPrice(), currentOrder
-					.getVolume());
-			orderList.add(order);
-		}
-
-		return orderList;
-	}
-
-	public void execute() {
-		// orderEntity.execute();
-	}
+	
+	
 
 	public static void main(String args[]) {
 		// AssetEntity asset = new AssetEntity("so xo", "SXS", 1, null, 0.05);
@@ -198,19 +73,15 @@ public class Order {
 
 	}
 
-	public void setAssetSymbol(String assetSymbol) {
-		this.assetSymbol = assetSymbol;
-	}
-
-	public String getAssetSymbol() {
-		return assetSymbol;
-	}
-
 	public void setOrderType(boolean orderType) {
 		this.orderType = orderType;
 	}
 
 	public boolean isOrderType() {
+		return orderType;
+	}
+	
+	public boolean getOrderType() {
 		return orderType;
 	}
 
@@ -236,6 +107,22 @@ public class Order {
 
 	public double getValue() {
 		return value;
+	}
+
+	public void setAsset(Asset asset) {
+		this.asset = new Asset(asset);
+	}
+
+	public Asset getAsset() {
+		return asset;
+	}
+
+	public void setMatched(boolean matched) {
+		this.matched = matched;
+	}
+
+	public boolean getMatched() {
+		return matched;
 	}
 
 }
